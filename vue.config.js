70d69f835f4db3b9fa6e8ab83c380838b883029d
qinxuanlong <1470681411@qq.com>
2024-07-11 13:48:28 +0800

    资源修改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    publicPath: '/tool/',
+    publicPath: '/myTool/',
     outputDir: 'docs',
     // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 
     assetsDir: "assets",
